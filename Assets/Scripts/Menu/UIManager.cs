d5e98e8a6fc97bc81f3587a242903bac5d527a71
kirillyakubov <kirillyakubov101@gmail.com>
2020-11-24 00:30:04 -0500

    Polish of terrain + UI + Menu start

---
+++ Assets/Scripts/Menu/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+	[SerializeField] AudioClip startButtonClip = null;
+	AudioSource audioSource;
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void StartGameButton()
+	{
+		StartCoroutine(StartGame());	
+	}
+
+	private IEnumerator StartGame()
+	{
+		if(startButtonClip == null)
+		{
+			yield return null;
+		}
+		audioSource.clip = startButtonClip;
+		audioSource.Play();
+		
+		yield return new WaitForSeconds(startButtonClip.length);
+
+		SceneManager.LoadScene(1);
+	}
+
+}
